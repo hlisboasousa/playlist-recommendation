3869515f36cc8eabbea1e93472c511a09a6f2557
Henrique <hlisboasousa@gmail.com>
2023-12-02 18:43:55 -0300

    adding cypress to pipeline

--- cypress/e2e/integration/clear-tracks.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Clear Tracks Test', () => {
-  it('should clear the track list', () => {
-    cy.visit('http://localhost:8000'); // Replace with your actual application URL
-    cy.get('#trackInput').type('Track1');
-    cy.get('.add-btn').click();
-    cy.get('.clear-btn').click();
-    cy.get('#trackList li').should('have.length', 0);
-  });
-});
